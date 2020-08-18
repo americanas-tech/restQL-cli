7828926c3a8b71e8a4bfae02641de3acddd9b581
Caio Ferreira <caiorcferreira@gmail.com>
2020-08-18 17:46:58 -0300

    refactor(cli): change cli to use a build subcommand

--- main.go
+++ main.go
@@ -18,28 +18,35 @@ func main() {
 func NewApp() *cli.App {
 	return &cli.App{
 		Name: "restql",
-		Usage: "Builds custom binaries for RestQL with the given plugins",
-		Flags: []cli.Flag{
-			&cli.StringSliceFlag{
-				Name: "with",
-				Aliases: []string{"w"},
-				Required: true,
-				Usage: "Specify the Go Module name of the plugin, can optionally set the version and a replace path: github.com/user/plugin[@version][=../replace/path]",
-			},
-			&cli.StringFlag{
-				Name: "output",
-				Aliases: []string{"o"},
-				Value: "./",
-				Usage: "Set the location where the final binary will be placed",
+		Usage: "Manage the development and building of plugins within RestQL",
+		Commands: []*cli.Command{
+			{
+				Name: "build",
+				Usage: "Builds custom binaries for RestQL with the given plugins",
+				Flags: []cli.Flag{
+					&cli.StringSliceFlag{
+						Name: "with",
+						Aliases: []string{"w"},
+						Required: true,
+						Usage: "Specify the Go Module name of the plugin, can optionally set the version and a replace path: github.com/user/plugin[@version][=../replace/path]",
+					},
+					&cli.StringFlag{
+						Name: "output",
+						Aliases: []string{"o"},
+						Value: "./",
+						Usage: "Set the location where the final binary will be placed",
+					},
+				},
+				Action: func(ctx *cli.Context) error {
+					withPlugins := ctx.StringSlice("with")
+					output := ctx.String("output")
+
+					restqlVersion := ctx.Args().Get(0)
+
+					return compilation.BuildRestQL(ctx.Context, withPlugins, restqlVersion, output)
+				},
 			},
 		},
-		Action: func(ctx *cli.Context) error {
-			withPlugins := ctx.StringSlice("with")
-			output := ctx.String("output")
 
-			restqlVersion := ctx.Args().Get(0)
-
-			return compilation.BuildRestQL(ctx.Context, withPlugins, restqlVersion, output)
-		},
 	}
 }
