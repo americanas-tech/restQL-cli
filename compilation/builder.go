5e741b60415aab38f1885bd352e922b0d294d487
Caio Ferreira <caiorcferreira@gmail.com>
2020-08-18 17:07:17 -0300

    refactor(compilation): extract plugin type to own file

--- compilation/builder.go
+++ compilation/builder.go
@@ -8,7 +8,6 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
-	"regexp"
 	"strings"
 )
 
@@ -98,43 +97,3 @@ func getRestqlVersion(ctx context.Context, env *Environment) (string, error) {
 
 	return moduleNameAndVersion[1], nil
 }
-
-var pluginInfoRegex = regexp.MustCompile("([^@=]+)@?([^=]*)=?(.*)")
-
-type Plugin struct {
-	ModulePath string
-	Version    string
-	Replace    string
-}
-
-func ParsePluginInfo(pluginInfo string) Plugin {
-	if pluginInfo == "" {
-		return Plugin{}
-	}
-
-	p := Plugin{}
-	matches := pluginInfoRegex.FindAllStringSubmatch(pluginInfo, -1)
-	if len(matches) < 1 {
-		return Plugin{}
-	}
-
-	submatches := matches[0]
-	if len(submatches) >= 2 {
-		mn := submatches[1]
-		p.ModulePath = mn
-	}
-
-	if len(submatches) >= 3 {
-		v := submatches[2]
-		p.Version = v
-	}
-
-	if len(submatches) >= 4 {
-		r := submatches[3]
-		p.Replace = r
-	}
-
-	return p
-}
-
-
---
+++ compilation/plugin.go
@@ -0,0 +1,41 @@
+package compilation
+
+import "regexp"
+
+var pluginInfoRegex = regexp.MustCompile("([^@=]+)@?([^=]*)=?(.*)")
+
+type Plugin struct {
+	ModulePath string
+	Version    string
+	Replace    string
+}
+
+func ParsePluginInfo(pluginInfo string) Plugin {
+	if pluginInfo == "" {
+		return Plugin{}
+	}
+
+	p := Plugin{}
+	matches := pluginInfoRegex.FindAllStringSubmatch(pluginInfo, -1)
+	if len(matches) < 1 {
+		return Plugin{}
+	}
+
+	submatches := matches[0]
+	if len(submatches) >= 2 {
+		mn := submatches[1]
+		p.ModulePath = mn
+	}
+
+	if len(submatches) >= 3 {
+		v := submatches[2]
+		p.Version = v
+	}
+
+	if len(submatches) >= 4 {
+		r := submatches[3]
+		p.Replace = r
+	}
+
+	return p
+}
